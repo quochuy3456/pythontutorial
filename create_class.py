0ad7bd4bc1112e8cda6bfb1b3ed9c555a4393f26
Do Quoc Huy <quochuy3456@gmail.com>
2021-01-01 22:04:54 +0700

    bt1 create function

--- create_class.py
+++ create_class.py
@@ -1,3 +1,34 @@
-A = [2, 4, 6, 7, 9, 0, 45, 3]
-
 # Viết hàm cộng thêm 1 vào các phần tử trong list
+
+
+# Viết code tính tài xỉu tử 3 con xúc xác có 6 mặt
+
+# from random import randint
+#
+# player = str(input("Nhap tai hoac xiu: ")).lower()
+# so_tien = int(input("Bao nhieu tien: "))
+#
+# rd = [randint(1, 6), randint(1, 6), randint(1, 6)]
+# _sum = sum(rd)
+# print(rd)
+# rs = "tai" if _sum >= 10 else "xiu"
+#
+# if player == rs:
+#     print("Win: ", so_tien + 0.7*so_tien)
+# else:
+#     print("Lose")
+
+A = [10, 2, 3, 4, 5]
+#
+# B = [x + 1 for x in A]
+# print(B)
+
+# import numpy as np
+# print(list(np.array(A) + 1))
+
+# print(list(map(lambda x: x+1, A)))
+
+def sum(gsdgsdfs):
+    return gsdgsdfs + 1
+
+print(list(map(sum, A)))
